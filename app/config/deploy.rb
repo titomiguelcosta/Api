45623f8ea20fdd50550a474bca6c9b67840caf87
Tito Miguel Costa <titomiguelcosta@gmail.com>
2014-12-21 14:11:04 +0000

    integrate capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "api"
+set :domain,      "#{application}.titomiguelcosta.com"
+set :deploy_to,   "/home/ubuntu/websites/api"
+set :app_path,    "app"
+
+set :repository,  "https://github.com/titomiguelcosta/Api.git"
+set :scm,         :git
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
+
+set :use_sudo, false
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
+set :vendors_mode, :install
+set :copy_vendors, true
+after "deploy", "deploy:cleanup"
