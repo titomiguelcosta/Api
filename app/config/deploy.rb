f76a4efa28f2a56c2b8da6de9dfabdf80ce19c59
Tito Miguel Costa <titomiguelcosta@gmail.com>
2014-12-22 21:38:21 +0000

    forced the prod environment when deploying

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -16,13 +16,18 @@ set  :keep_releases,  3
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
 
+set :symfony_env_prod, "prod"
+
 set :user, "ubuntu"
 set :use_sudo, false
+
 set :shared_files, ["app/config/parameters.yml"]
 set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+
 set :use_composer, true
 set :composer_options,  "--no-dev --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction"
 set :update_vendors, true
 set :vendors_mode, "install"
 set :copy_vendors, true
+
 after "deploy", "deploy:cleanup"
