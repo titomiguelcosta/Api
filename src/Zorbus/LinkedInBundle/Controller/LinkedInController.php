53c51ac640f2582ec8c2486e5c20f4e193e68c34
Tito Miguel Costa <titomiguelcosta@gmail.com>
2014-12-23 17:57:34 +0000

    added event when linkedin token is retrieved successfully

--- src/Zorbus/LinkedInBundle/Controller/LinkedInController.php
+++ src/Zorbus/LinkedInBundle/Controller/LinkedInController.php
@@ -6,9 +6,9 @@ use GuzzleHttp\Message\Response as GuzzleResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Zorbus\LinkedInBundle\Event\LinkedInTokenEvent;
 
 class LinkedInController extends Controller
 {
@@ -19,11 +19,11 @@ class LinkedInController extends Controller
     public function authorizeAction(Request $request)
     {
         /** @var \Zorbus\LinkedIn\Manager $manager */
-        $manager = $this->get('zorbus.linkedin.manager');
+        $manager = $this->get('zorbus_linkedin.manager');
 
-        $key = $this->container->getParameter('zorbus.linkedin.key');
-        $scope = $this->container->getParameter('zorbus.linkedin.scope');
-        $url = $this->generateUrl('zorbus.linkedin.authenticate', [], true);
+        $key = $this->container->getParameter('zorbus_linkedin.key');
+        $scope = $this->container->getParameter('zorbus_linkedin.scope');
+        $url = $this->generateUrl('zorbus_linkedin.authenticate', [], true);
 
         $manager->authorize($key, $scope, $url);
     }
@@ -35,18 +35,24 @@ class LinkedInController extends Controller
     public function authenticateAction(Request $request)
     {
         /** @var \Zorbus\LinkedIn\Manager $manager */
-        $manager = $this->get('zorbus.linkedin.manager');
+        $manager = $this->get('zorbus_linkedin.manager');
 
         $code = $request->query->get('code');
         $state = $request->query->get('state');
-        $secret = $this->container->getParameter('zorbus.linkedin.secret');
-        $key = $this->container->getParameter('zorbus.linkedin.key');
-        $redirectUrl =  $this->generateUrl('zorbus.linkedin.authenticate', [], true);
+        $secret = $this->container->getParameter('zorbus_linkedin.secret');
+        $key = $this->container->getParameter('zorbus_linkedin.key');
+        $redirectUrl = $this->generateUrl('zorbus_linkedin.authenticate', [], true);
 
         $response = $manager->authenticate($code, $state, $secret, $key, $redirectUrl);
 
         if ($response instanceof GuzzleResponse) {
-            return new JsonResponse(json_encode($response->getBody()->getContents()));
+            $data = json_encode($response->getBody()->getContents());
+
+            $event = new LinkedInTokenEvent($data['access_token'], new \DateTime('+' . $data['expires_at'] . ' seconds'));
+
+            $this->get('event_dispatcher')->dispatch('zorbus_linkedin.access_token', $event);
+
+            return new Response('Retrieved access token. Ready to go.');
         }
 
         return new Response($response);
---
+++ src/Zorbus/LinkedInBundle/Event/LinkedInTokenEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zorbus\LinkedInBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use DateTime;
+
+class LinkedInTokenEvent extends Event
+{
+    private $accessToken;
+    private $expiresAt;
+
+    public function __construct($accessToken, DateTime $expiredAt)
+    {
+        $this->accessToken = $accessToken;
+        $this->expiresAt;
+    }
+
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+}
